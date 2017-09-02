b91e57d90a9f283c689359db6c05f7fbf6fa4720
Dave <dgiles54@gmail.com>
2017-09-02 14:21:56 -0500

    updated files

--- LearningPhaser/state1.js
+++ LearningPhaser/state1.js
@@ -2,6 +2,9 @@ var demo = {};
 demo.state1 = function () {};
 demo.state1.prototype = {
     preload: function () {},
-    create: function () {},
+    create: function () {
+        game.stage.backgroundColor = '#ff0000';
+        console.log('state1');
+    },
     update: function () {}
-}
+};
