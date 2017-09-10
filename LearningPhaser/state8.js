b83e33b613eb18416e691991e4d855dd4084bc4b
Dave <dgiles54@gmail.com>
2017-09-10 17:56:27 -0500

    finished breakout game

--- LearningPhaser/state8.js
+++ LearningPhaser/state8.js
@@ -1,9 +1,52 @@
+var text;
+
+WebFontConfig = {
+    google: {
+        families: ['Indie Flower']
+    }
+};
+
 demo.state8 = function () {};
 demo.state8.prototype = {
-    preload: function () {},
-    create: function () {
-        game.stage.backgroundColor = 'ffffff';
-        addChangeStateEventListeners();
+    preload: function () {
+        game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js')
     },
-    update: function () {}
+    create: function () {
+        game.stage.backgroundColor = '#99e6e6';
+        addChangeStateEventListeners();
+
+        text = "RICK: Listen, Morty, I hate to break it to you but what people call 'love' is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science."
+
+        this.spellOutText(100, 100, 1000, text, 40, 40, '#ffffff', 'Indie Flower');
+    },
+    spellOutText: function (x, y, width, text, fontSize, speed, fill, font) {
+        var sentence = game.add.text(x, y, '', {
+            fontSize: fontSize + 'px',
+            fill: fill,
+            font: font
+        });
+        var currentLine = game.add.text(10, 10, '', {
+            fontSize: fontSize + 'px',
+            fill: fill,
+            font: font
+        });
+        currentLine.alpha = 0;
+        var loop = game.time.events.loop(speed, addChar);
+
+        var index = 0;
+
+        function addChar() {
+            sentence.text += text[index];
+            currentLine.text += text[index];
+
+            if (currentLine.width > width && text[index] == ' ') {
+                sentence.text += '\n';
+                currentLine.text = '';
+            }
+            if (index >= text.length - 1) {
+                game.time.events.remove(loop);
+            }
+            index++;
+        }
+    }
 };
--- LearningPhaser/state9.js
+++ LearningPhaser/state9.js
@@ -1,9 +1,40 @@
+var hsText = [];
+var hs = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+var config;
+var database;
+
 demo.state9 = function () {};
 demo.state9.prototype = {
     preload: function () {},
     create: function () {
         game.stage.backgroundColor = '#ff0000';
         addChangeStateEventListeners();
+
+        config = {
+            apiKey: "AIzaSyDpFr80tKIzxvsalzvR1W6Sav-Uurb54SY",
+            authDomain: "phaserdemotest-9504f.firebaseapp.com",
+            databaseURL: "https://phaserdemotest-9504f.firebaseio.com",
+            projectId: "phaserdemotest-9504f",
+            storageBucket: "phaserdemotest-9504f.appspot.com",
+            messagingSenderId: "64532902339"
+        };
+        firebase.initializeApp(config);
+
+        // get a reference to database service
+        database = firebase.database();
+
+        for (var i = 1; i < 11; i++) {
+            game.add.text(400, 10 + (i * 80), i + '.', {
+                fontSize: '40px'
+            }).anchor.setTo(1, 0);
+        }
+
+        for (var i = 0; i < 10; i++) {
+            hsText[i] = game.add.text(410, 10 + ((i + 1) * 80), hs[i], {
+                fontSize: '40px'
+            });
+        }
+
     },
     update: function () {}
 };
